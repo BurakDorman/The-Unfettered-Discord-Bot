e63e03d0664bce5821501e6f060cce49cc57d360
BurakDorman <47524842+BurakDorman@users.noreply.github.com>
2022-09-20 13:50:47 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,33 @@
+const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent, //IMPORTANT: make sure you enable "Message Content Intent" in the dev portal!
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.DirectMessages,
+	]
+});
+const { Captcha } = require("discord.js-captcha");
+
+const captcha = new Captcha(client, {
+	guildID: "804098728952594432",
+	roleID: "809130374587351111", //optional
+	channelID: "804098950420103219", //optional
+	sendToTextChannel: true, //optional, defaults to false
+	addRoleOnSuccess: true, //optional, defaults to true. whether you want the bot to add the role to the user if the captcha is solved
+	kickOnFailure: true, //optional, defaults to true. whether you want the bot to kick the user if the captcha is failed
+	caseSensitive: false, //optional, defaults to true. whether you want the captcha responses to be case-sensitive
+	attempts: 3, //optional, defaults to 1. number of attempts before captcha is considered to be failed
+	timeout: 120000, //optional, defaults to 60000. time the user has to solve the captcha on each attempt in milliseconds
+	showAttemptCount: true, //optional, defaults to true. whether to show the number of attempts left in embed footer
+	//customPromptEmbed: new EmbedBuilder(), //customise the embed that will be sent to the user when the captcha is requested
+	//customSuccessEmbed: new EmbedBuilder(), //customise the embed that will be sent to the user when the captcha is solved
+	//customFailureEmbed: new EmbedBuilder(), //customise the embed that will be sent to the user when they fail to solve the captcha
+});
+
+client.on("guildMemberAdd", async member => {
+	captcha.present(member);
+});
+
+client.login("MTAyMDc2Nzc3MTEzMjQ0ODg4MA.Gw2ldo.AV2aDLpCHovMOJWI7yLIcM4g-gnd0NGgqULd04")
